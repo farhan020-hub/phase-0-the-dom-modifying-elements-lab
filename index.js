b269924944438ed64dd8c1a392ce82ab1d242129
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-14 23:45:36 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+const headerElement = document.querySelector("h1");
+const newHeader = headerElement;
+
+document.createElement("id");
+newHeader.id = "victory";
+newHeader.textContent = "Abdiaziz is the champion";
